ae9463195c761d232c8cc9513d76f368642cd4c0
steve <steve@ubuntu.(none)>
2012-03-06 14:56:27 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studemo::Application.config.secret_token = 'ba7dc2382f840f06cdaa4c9bf874b2818498c6c3bfb2914b9ec935a8d00f8e0e1f305e8bf6578143f77d56564c4ab862eb1ee4687ad123694e08cd071e350931'
